b46888f506cc94f8f555ac36c9f84393b69fb23e
Semert <semert2121@gmail.com>
2020-09-21 12:18:15 +0300

    css

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -17,8 +17,14 @@ const Sidebar = () => {
     <div className="sidebar">
       <TwitterIcon />
 
-      <SidebarOption text="home" />
-      <SidebarOption text="deneme" />
+      <SidebarOption Icon={HomeIcon} text="home" />
+      <SidebarOption Icon={SearchIcon} text="Explore" />
+      <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+      <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOption Icon={ListAltIcon} text="Lists" />
+      <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
     </div>
   );
 };
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,9 +1,11 @@
+import { Icon } from "@material-ui/core";
 import React from "react";
 import "./SidebarOption.css";
 
-function SidebarOption({ text, icon }) {
+function SidebarOption({ text, Icon }) {
   return (
     <div className="sidebarOption">
+      <Icon />
       <h2>{text}</h2>
     </div>
   );
